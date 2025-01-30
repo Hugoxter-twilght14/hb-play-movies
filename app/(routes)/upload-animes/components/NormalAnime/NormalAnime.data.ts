bfbfeac2c117cc52c14a4e5002b3faa4ef9dcacf
HugoBG14 <al21510374@tapachula.tecnm.mx>
2025-01-29 22:34:48 -0600

    Anime Ranma 1/2 uploaded in setion animes

--- app/(routes)/upload-animes/components/NormalAnime/NormalAnime.data.ts
+++ app/(routes)/upload-animes/components/NormalAnime/NormalAnime.data.ts
@@ -1,10 +1,10 @@
 export const dataAnime = [
     {
-      title: "Attack on Titan",
+      title: "Ranma ½",
       thumbnailUrl:
-        "https://raw.githubusercontent.com/Hugoxter-twilght14/images-for-playMovies/refs/heads/main/anime-images/atot-1.jpg",
-      genre: ["Action", "Drama"],
-      age: "16",
+        "https://raw.githubusercontent.com/Hugoxter-twilght14/images-for-playMovies/refs/heads/main/anime-images/Ranma12-t1.jpg",
+      genre: ["Acción", "Comedia", "romántica", "Aventura", "Ecchi"],
+      age: "13",
       duration: "Serie",
       trailerVideo: "https://youtu.be/MGRm4IzK1SQ",
       seasons: [
@@ -13,54 +13,75 @@ export const dataAnime = [
           episodes: [
             {
               number: 1,
-              title: "To You, in 2000 Years",
+              title: "La llegada de Ranma",
               duration: "24 min",
-              videoUrl: "https://youtu.be/MGRm4IzK1SQ",
+              videoUrl: "https://www.dropbox.com/home/ANIMES/RANM4_1/2?preview=01-La+llegada+de+Ranma.mp4",
             },
             {
               number: 2,
-              title: "The Fall of Shiganshina",
-              duration: "23 min",
-              videoUrl: "https://youtu.be/MGRm4IzK1SQ",
-            },
-          ],
-        },
-        {
-          number: 2,
-          episodes: [
-            {
-              number: 1,
-              title: "Beast Titan",
-              duration: "25 min",
-              videoUrl: "https://youtu.be/MGRm4IzK1SQ",
-            },
-          ],
-        },
-      ],
-    },
-    {
-      title: "Demon Slayer",
-      thumbnailUrl:
-        "https://raw.githubusercontent.com/Hugoxter-twilght14/images-for-playMovies/refs/heads/main/anime-images/ds-1.jpg",
-      genre: ["Adventure", "Fantasy"],
-      age: "14",
-      duration: "Serie",
-      trailerVideo: "https://youtu.be/mkRtrv3iulk?si=8nU1fx-1wnqpzD5r",
-      seasons: [
-        {
-          number: 1,
-          episodes: [
-            {
-              number: 1,
-              title: "Cruelty",
+              title: "Odio a los hombres",
               duration: "24 min",
-              videoUrl: "https://youtu.be/mkRtrv3iulk?si=8nU1fx-1wnqpzD5r",
+              videoUrl: "https://www.dropbox.com/home/ANIMES/RANMA?preview=02+-Odio+a+los+hombres.mp4",
             },
             {
-              number: 2,
-              title: "Trainer Sakonji Urokodaki",
-              duration: "23 min",
-              videoUrl: "https://youtu.be/mkRtrv3iulk?si=8nU1fx-1wnqpzD5r",
+              number: 3,
+              title: "Porque le gusta alguien",
+              duration: "24 min",
+              videoUrl: "https://www.dropbox.com/home/ANIMES/RANMA?preview=03-Porque+le+gusta+alguien.mp4",
+            },
+            {
+              number: 4,
+              title: "El hombre que perseguia a Ranma",
+              duration: "24 min",
+              videoUrl: "https://www.dropbox.com/home/ANIMES/RANMA?preview=04-El+hombre+que+persegu%C3%ADa+a+Ranma.mp4",
+            },
+            {
+              number: 5,
+              title: "Eres odiosa",
+              duration: "24 min",
+              videoUrl: "https://www.dropbox.com/home/ANIMES/RANMA?preview=05-Eres+odiosa.mp4",
+            },
+            {
+              number: 6,
+              title: "Kodachi la rosa negra",
+              duration: "24 min",
+              videoUrl: "https://www.dropbox.com/home/ANIMES/RANMA?preview=06-Kodachi+de+la+rosa+negra.mp4",
+            },
+            {
+              number: 7,
+              title: "Una disputada lucha gimnástica",
+              duration: "24 min",
+              videoUrl: "https://www.dropbox.com/home/ANIMES/RANMA?preview=07-Una+disputada+lucha+gimn%C3%A1stica.mp4",
+            },
+            {
+              number: 8,
+              title: "Mi querido charlotte",
+              duration: "24 min",
+              videoUrl: "https://www.dropbox.com/home/ANIMES/RANMA?preview=08-Mi+querido+Charlotte.mp4",
+            },
+            {
+              number: 9,
+              title: "Jamas te soltaré",
+              duration: "24 min",
+              videoUrl: "https://www.dropbox.com/home/ANIMES/RANM4_1/2?preview=09-Jam%C3%A1s+te+soltar%C3%A9.mp4",
+            },
+            {
+              number: 10,
+              title: "El beso de la muerte",
+              duration: "24 min",
+              videoUrl: "https://www.dropbox.com/home/ANIMES/RANM4_1/2?preview=10-El+beso+de+la+muerte.mp4",
+            },
+            {
+              number: 11,
+              title: "Wo ai ni",
+              duration: "24 min",
+              videoUrl: "https://www.dropbox.com/home/ANIMES/RANM4_1/2?preview=11-Wo+ai+ni.mp4",
+            },
+            {
+              number: 12,
+              title: "El champú amnésico",
+              duration: "24 min",
+              videoUrl: "https://www.dropbox.com/home/ANIMES/RANM4_1/2?preview=12-El+champ%C3%BA+amn%C3%A9sico.mp4",
             },
           ],
         },
