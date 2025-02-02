6d5f755ce977272e9bfd5c4e502c79d221414bc9
HugoBG14 <al21510374@tapachula.tecnm.mx>
2025-02-02 01:16:17 -0600

    fix: add new icon for the app in the Navbars for all page

--- components/Shared/Logo/Logo.tsx
+++ components/Shared/Logo/Logo.tsx
@@ -1,9 +1,11 @@
+import Image from "next/image";
 import Link from "next/link";
 
 export function Logo() {
   return (
     <Link href="/" className="font-extrabold text-2xl text-[#00FFFF]">
-      PlayMovies
+      <Image src="/iconos/icono-playmovies.jpg" alt="Logo PlayMovies" title="PlayMovies" width={35} height={35}/>
+      <p className="mt-[-38px] mx-[40px]">PlayMovies</p>
     </Link>
   );
 }
