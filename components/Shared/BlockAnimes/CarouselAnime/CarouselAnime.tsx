fe3566427f8c852fa326a8f77db733b53f4ac806
HugoBG14 <borrazhugo14@gmail.com>
2025-04-23 20:42:54 -0600

    carousel fixed in view mobiles and pc

--- components/Shared/BlockAnimes/CarouselAnime/CarouselAnime.tsx
+++ components/Shared/BlockAnimes/CarouselAnime/CarouselAnime.tsx
@@ -23,31 +23,32 @@ interface Props {
 export function CarouselAnime({ animes }: Props) {
   return (
     <div className="w-full overflow-x-auto max-w-full">
-      <div className="flex gap-4 px-4 py-4 overflow-x-auto scroll-smooth snap-x">
+      <div className="flex gap-4 px-4 py-6 overflow-x-auto scroll-smooth snap-x scrollbar-hide">
         {animes.map((anime) => (
           <div
             key={anime.isMoreCard ? "ver-mas" : anime.id}
-            className="snap-start shrink-0 w-[85%] sm:w-[50%] md:w-[33%] lg:w-[25%] xl:w-[16.6%] bg-zinc-800 text-white rounded-md overflow-hidden border border-zinc-700"
+            className="snap-start shrink-0 w-[85%] sm:w-[50%] md:w-[33%] lg:w-[25%] xl:w-[16.6%] bg-zinc-800 text-white rounded-2xl overflow-hidden border border-zinc-700 shadow-md md:hover:scale-[1.03] transition-transform duration-300"
           >
-            {anime.isMoreCard ? (
+           {anime.isMoreCard ? (
               <Link href="/animes">
-                <div className="w-full h-[200px] flex items-center justify-center text-lg font-bold bg-gradient-to-br from-slate-800 to-slate-900">
-                  Ver más
-                </div>
+              <div className="w-full h-full flex items-center justify-center text-lg font-bold text-white bg-gradient-to-br from-slate-700 to-slate-900 rounded-2xl">
+                Ver más
+              </div>
               </Link>
             ) : (
-              <>
+                <>
                 <img
                   src={anime.thumbnailUrl}
                   alt={anime.title}
-                  className="w-full h-[200px] object-cover"
                   loading="lazy"
+                  className="w-full aspect-[2/3] object-cover"
                 />
-                <div className="p-3">
-                  <h4 className="text-sm font-semibold truncate mb-1">{anime.title}</h4>
+                
+                <div className="p-4 flex flex-col gap-2">
+                  <h4 className="text-base font-semibold truncate">{anime.title}</h4>
 
                   {anime.description && (
-                    <p className="text-gray-300 text-xs mb-2 line-clamp-3">
+                    <p className="text-gray-300 text-sm line-clamp-3">
                       {anime.description}
                     </p>
                   )}
