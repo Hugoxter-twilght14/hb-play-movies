6438f87bc52e45abd02c02087eb4b7b300eacd30
HugoBG14 <borrazhugo14@gmail.com>
2025-05-03 11:56:10 -0600

    fix error bug refresh infinite in login, register and olvide-password

--- components/PerfilInitializer/PerfilInitializer.tsx
+++ components/PerfilInitializer/PerfilInitializer.tsx
@@ -1,9 +1,20 @@
-// components/PerfilInitializer.tsx
-"use client"
+// PerfilInitializer.tsx
+"use client";
 
-import { useInitPerfil } from "@/hooks/useInitPerfil"
+import { useInitPerfil } from "@/hooks/useInitPerfil";
+import { usePathname } from "next/navigation";
 
 export function PerfilInitializer() {
-  useInitPerfil()
-  return null
+  const pathname = usePathname();
+
+  const isPublic =
+    pathname.startsWith("/login") ||
+    pathname.startsWith("/register") ||
+    pathname.startsWith("/olvide-password") ||
+    pathname.startsWith("/reset-password");
+
+  if (isPublic) return null;
+
+  useInitPerfil();
+  return null;
 }
